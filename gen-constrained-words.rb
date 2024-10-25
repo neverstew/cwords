b27182b173ca4eee45b78b6f69dffd4178153d02
Matt Williams <matthew@virtueimpact.com>
2024-10-25 13:39:21 +1300

    Handles multiple constraints per word

--- gen-constrained-words.rb
+++ gen-constrained-words.rb
@@ -6,11 +6,6 @@ db = SQLite3::Database.new "words.db"
 
 @letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
 
-# Example:
-# cat = constraints("words", a: "c", b: "a", c: "t", d: "s")
-# rows = db.execute "select * from words where #{cat[0]}", *cat[1]
-# any_word = constraints("words", a: :any, b: :any, c: :any, d: :any)
-# rows = db.execute "select * from words where #{any_word[0]}", *any_word[1]
 def constraints(table_name, **args)
   conditions = []
   values = []
@@ -48,9 +43,9 @@ end
 @words = parse_structure %w[
 a1/d1 a1 a1/d2 a1 a1/d3
 d1    .  d2    .  d3 
-d1    .  d2    .  d3
+d1    .  d2/a4 a4 d3/a4
 .     .  d2    .  .
-a4    a4 d2/a4 a4 a4
+a5    a5 d2/a5 a5 a5
 ]
 
 select = ->(word_key) { "select * from words as #{word_key}" }
@@ -58,13 +53,20 @@ where = ->(word_key) {
   letters = @words[word_key].map{ |letter| [letter.to_sym, :any] }.to_h
   "where #{constraints(word_key, **letters)[0]}"
 }
-join = ->(this_word_key, other_word_key, letter) {
+join = ->(this_word_key, *joins) {
   idx_this = @words.keys.index(this_word_key)
-  idx_other = @words.keys.index(other_word_key)
-  difference = idx_this - idx_other
-  relative_cte_spacing = difference > 1 ? ":#{difference - 1}" : ""
   prev_word_key = @words.keys[idx_this - 1]
-  "join #{prev_word_key} on #{prev_word_key}.\"#{letter}#{relative_cte_spacing}\" = #{this_word_key}.#{letter}"
+
+  join_conditions =
+    joins.map do |other_word_key, letter|
+      idx_other = @words.keys.index(other_word_key)
+      difference = idx_this - idx_other
+      relative_cte_spacing = difference > 1 ? ":#{difference - 1}" : ""
+      "#{prev_word_key}.\"#{letter}#{relative_cte_spacing}\" = #{this_word_key}.#{letter}"
+    end
+    .join(' and ')
+
+  "join #{prev_word_key} on #{join_conditions}"
 }
 
 query = <<-SQL
@@ -73,23 +75,28 @@ query = <<-SQL
     #{where.("a1")}
   ), d1 as (
     #{select.("d1")}
-    #{join.("d1", "a1", "a")}
+    #{join.("d1", ["a1", "a"])}
     #{where.("d1")}
   ), d2 as (
     #{select.("d2")}
-    #{join.("d2", "a1", "c")}
+    #{join.("d2", ["a1", "c"])}
     #{where.("d2")}
   ), d3 as (
     #{select.("d3")}
-    #{join.("d3", "a1", "e")}
+    #{join.("d3", ["a1", "e"])}
     #{where.("d3")}
   ), a4 as (
     #{select.("a4")}
-    #{join.("a4", "d2", "w")}
+    #{join.("a4", ["d2", "m"], ["d3", "o"])}
     #{where.("a4")}
+  ), a5 as (
+    #{select.("a5")}
+    #{join.("a5", ["d2", "w"])}
+    #{where.("a5")}
   )
-  select * from a4
+  select * from a5
   limit 1
+  offset 0
 SQL
 File.write "latest-query.sql", query
 
@@ -103,7 +110,7 @@ def print_crossword(result)
       final_table[i] = table[i]
     end
   end
-  final_table.each_slice(5){ |row| pp row }
+  final_table.map{ |letter| letter || '.' }.each_slice(5){ |row| pp row.join(' ') }
 end
 
 print_crossword(rows[0])
