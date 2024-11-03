e9ca6873b003f106ae71db1eaeac215c7796b08c
Matt Williams <matthew@virtueimpact.com>
2024-11-04 12:40:40 +1300

    Words greyed-out when filled-in

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -51,7 +51,10 @@ const Words = () => {
 
 const Word = ({ id, word }: { id: keyof GameState['words'], word: GameState['words'][keyof GameState['words']] }) => {
   const [state, dispatch] = useGameContext();
-  const classes = clsx(id === state.selectedWord && "font-bold");
+  const classes = clsx(
+    id === state.selectedWord && "font-bold",
+    state.words[id].range.map(i => state.letters[i]).every(char => char.match(/[a-zA-Z]/)) && "text-gray-400",
+  );
 
   return (
     <div className={classes}>
