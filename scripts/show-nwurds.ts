8d8f08f60f213656dc5e5a37d5723dc377f3b026
Matt Williams <matthew@virtueimpact.com>
2024-11-22 14:21:37 +1300

    Scratch script playing around with transformation ideas

--- scripts/show-nwurds.ts
+++ scripts/show-nwurds.ts
@@ -1,17 +1,13 @@
 const [_bun, _file, ...clues] = process.argv;
 let clue = clues.join(' ');
 // from https://www.theguardian.com/crosswords/cryptic/29525#17-down
-clue ||= "Revolutionary device that reproduces itself when up-ended (7)";
+clue ||= "Hamlet's struggle to hold bitterness back (7)";
 
 const removeCount = (sentence: string) => sentence.trim().replace(/\(\d+\)$/, '').trim();
+clue = removeCount(clue)
+
 const splitSentence = (sentence: string) => sentence.split(/\s+/);
 
-const toWurd = (words: string[], start: number) => {
-    return {
-        wurd: words,
-        start,
-    }
-}
 /**
  * An n-wurd is like an n-gram, but for words.
  * e.g. bi-wurds for "this and that" are:
@@ -20,7 +16,23 @@ const toWurd = (words: string[], start: number) => {
  * 
  * We also retain information about the position of the wurd in the original context given
  */
-type Wurd = ReturnType<typeof toWurd>
+export type Wurd = {
+    wurd: string[];
+    start: number;
+    end: number;
+}
+
+const toLength = (x: string) => x.length;
+const totalWordLength = (words: string[]) => words.map(toLength).reduce(sum, 0);
+
+const sum = (el: number, sum: number): number => sum + el;
+export const toWurd = (words: string[], start: number) => {
+    return {
+        wurd: words,
+        start,
+        end: start + totalWordLength(words),
+    }
+}
 
 type nWurdsAccumulator = {
     currentStartIndex: number;
@@ -48,7 +60,7 @@ function nWurds(
 }
 
 export function allWurds(words: string | string[]) {
-    if (typeof words === 'string') return allWurds(splitSentence(removeCount(words)));
+    if (typeof words === 'string') return allWurds(splitSentence(words));
 
     const acc: nWurdsAccumulator = {
         currentStartIndex: 0,
@@ -61,19 +73,61 @@ export function allWurds(words: string | string[]) {
     return acc.wurds;
 }
 
-const result = allWurds(
-    splitSentence(
-        removeCount(clue)
-    ),
-)
+// print allWurds
+// for (const chunk of result) {
+//     console.info(chunk.wurd.join(' ').padStart(chunk.start + chunk.wurd.join(' ').length, ' '));
+// }
 
-const midpoint = Math.floor(removeCount(clue).length / 2);
-const byDistanceFromMidpoint = (a: Wurd, b: Wurd) => {
-    const distanceA = a.start - midpoint;
-    const distanceB = b.start - midpoint;
-    return distanceA - distanceB;
+// calculate valid combinations of wurds
+function validCombinationsForClue(clue: string) {
+    const clueWords = splitSentence(clue);
+    const numWords = clueWords.length;
+    const numSplitPositions = numWords - 1;
+    const splitBinaryMask = new Array(numSplitPositions).fill(1).join('')
+    const splitPositionsBinary = splitBinaryMask.length === 0 ? 0 : parseInt(splitBinaryMask, 2)
+    const splits =
+        new Array(splitPositionsBinary)
+            .fill(null)
+            .map((_, n) => n)
+            .map(n => n.toString(2).padStart(numSplitPositions, '0'))
+            .map(mask => mask.split(''))
+
+    const acc = []
+    for (const mask of splits) {
+        const chunks: string[][] = []
+        let currentChunk: string[] = [];
+        for (let i = 0; i < numWords; i++) {
+            const word = clueWords[i];
+            currentChunk.push(word);
+
+            const shouldSplitAfter = mask[i] === '1';
+            if (shouldSplitAfter) {
+                chunks.push(currentChunk);
+                currentChunk = [];
+            }
+        }
+        chunks.push(currentChunk);
+        acc.push(chunks);
+    }
+    acc.push(clueWords.map(w => [w]))
+
+    return acc;
 }
 
-for (const chunk of result.sort(byDistanceFromMidpoint)) {
-    console.info(chunk.wurd.join(' ').padStart(chunk.start + chunk.wurd.join(' ').length, ' '));
-}
+// print all valid combos of wurds
+// const combos = validCombinationsForClue(clue)
+// combos.forEach(console.info)
+
+let definition: string;
+const define = (clue: string, part: string) => { definition = part; return clue.replace(new RegExp(`(^${part}|${part}$)`), '').trim() }
+const synonym = (clue: string, part: string, replacement: string) => clue.replace(part, replacement).trim()
+const reversal = (clue: string, part: string, whole: string) => clue.replace(whole, part.split('').reverse().join('')).trim()
+const insertion = (clue: string, part: string, replacement: string) => synonym(clue, part, replacement);
+let step = clue;
+step = define(step, "Hamlet's");
+step = synonym(step, "struggle", "VIE")
+step = synonym(step, "bitterness", "GALL")
+step = reversal(step, "GALL", "GALL back")
+step = insertion(step, "VIE to hold LLAG", "VILLAGE")
+
+console.info(definition! + " = " + step)
